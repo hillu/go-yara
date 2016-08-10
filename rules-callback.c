0245f2a697620551b7baa58ef58dad18c7d96dcf
Hilko Bengen <bengen@hilluzination.de>
2016-08-11 00:52:13 +0200

    Explain #ifdef

--- rules-callback.c
+++ rules-callback.c
@@ -38,6 +38,7 @@ int rules_callback(int message, void *message_data, void *user_data) {
     yr_rule_strings_foreach(rule, string) {
       yr_string_matches_foreach(string, m) {
 #if YR_VERSION_HEX >= 0x030500
+        /* YR_MATCH members have been renamed in YARA 3.5 */
         addString(user_data, string->identifier, m->offset, m->data, (int)m->data_length);
 #else
         addString(user_data, string->identifier, m->offset, m->data, (int)m->length);
