3391bf9d7610a1cb389e0d4023768fe0ada5d5ed
Mike Wiacek <mjwiacek@chronicle.security>
2018-09-05 13:15:47 -0700

    Update tests to use a writable temp directory
    Ensure that when compiling the sample rule, it's stored in a writable temp directory.

--- main_test.go
+++ main_test.go
@@ -1,17 +1,32 @@
 package yara
 
 import (
+	"io/ioutil"
+	"log"
 	"os"
 	"testing"
 )
 
+var compiledTestRulesPath string
+
 func TestMain(m *testing.M) {
-	if r, err := Compile(`rule test : tag1 { meta: author = "Hilko Bengen" strings: $a = "abc" fullword condition: $a }`, nil); err != nil {
-		os.Exit(1)
-	} else if err = r.Save("testrules.yac"); err != nil {
-		os.Exit(1)
+	r, err := Compile(`rule test : tag1 { meta: author = "Hilko Bengen" strings: $a = "abc" fullword condition: $a }`, nil)
+	if err != nil {
+		log.Fatalf("Compile: %v", err)
 	}
+
+	f, err := ioutil.TempFile("", "testrules.yac")
+	if err != nil {
+		log.Fatalf("ioutil.TempFile: %v", err)
+	}
+	compiledTestRulesPath = f.Name()
+
+	if err := r.Save(compiledTestRulesPath); err != nil {
+		os.Remove(compiledTestRulesPath)
+		log.Fatalf("Save(%q): %v", compiledTestRulesPath, err)
+	}
+
 	rc := m.Run()
-	os.Remove("testrules.yac")
+	os.Remove(compiledTestRulesPath)
 	os.Exit(rc)
 }
