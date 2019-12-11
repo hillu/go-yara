6f554f86049e3b58c69bafcaaa9101434f0c135e
Pim Trouerbach <pim.trouerbach1@gmail.com>
2019-12-11 10:48:53 -0800

    Update rule.go

--- rule.go
+++ rule.go
@@ -79,10 +79,10 @@ static void string_matches(YR_STRING* s, const YR_MATCH *matches[], int *n) {
 	const YR_MATCH *match;
 	int i = 0;
     
-    if (!matches) {
-	*n = 0;
-	return;
-    }
+    	if (!matches) {
+		*n = 0;
+		return;
+    	}
 
 	yr_string_matches_foreach(s, match) {
 		if (i < *n)
