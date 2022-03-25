309929481e1e5074241053571827951094ca7f50
Hilko Bengen <bengen@hilluzination.de>
2022-03-25 23:36:21 +0100

    Replace cbpool...: Fix botched non-legacy version

--- handle.go
+++ handle.go
@@ -8,4 +8,8 @@ import "runtime/cgo"
 
 type cgoHandle cgo.Handle
 
-func cgoNewHandle(v interface{}) cgoHandle { return cgo.NewHandle(v) }
+func (h cgoHandle) Value() interface{} { return cgo.Handle(h).Value() }
+
+func (h cgoHandle) Delete() { cgo.Handle(h).Delete() }
+
+func cgoNewHandle(v interface{}) cgoHandle { return cgoHandle(cgo.NewHandle(v)) }
